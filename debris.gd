285b717cfa9f7560821858dc3fa0204c200fd31b
ahpham123 <ahpham123@csu.fullerton.edu>
2025-04-13 12:14:06 -0700

    fixed func
    fixed func

--- debris.gd
+++ debris.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+
 @export var move_speed: float = 50.0
 @export var move_direction: int = 1  # 1 for right, -1 for left
 @export var move_down_amount: float = 10.0
@@ -6,18 +7,23 @@ extends CharacterBody2D
 @export var boundary_right: float = 1024
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var collision_shape = $CollisionShape2D
+@onready var hit_detection_area = $HitDetectionArea
 
 var available_animations = ["stud", "thruster", "trash", "curry", "ball"]
+var is_alive = true
 
 func _ready():
 	animated_sprite.play(available_animations.pick_random())
+	# Connect area_entered signal if using Area2D for detection
+	if hit_detection_area:
+		hit_detection_area.area_entered.connect(_on_area_entered)
 	
 func _physics_process(delta: float) -> void:
+	if not is_alive:
+		return
+	
 	# Horizontal movement
-	var velocity = Vector2(move_speed * move_direction, 0)
-	move_and_slide()
-
-	# Move manually instead of using velocity
 	position.x += move_speed * move_direction * delta
 
 	# Reverse direction if reaching screen edge
@@ -27,3 +33,24 @@ func _physics_process(delta: float) -> void:
 	
 	if not animated_sprite.is_playing():
 		animated_sprite.play(available_animations.pick_random())
+
+func _on_area_entered(area):
+	# Check if the entering area is a laser
+	if area.is_in_group("lasers"):
+		destroy()
+
+func destroy():
+	if not is_alive:
+		return
+	
+	is_alive = false
+	# Disable collisions
+	collision_shape.set_deferred("disabled", true)
+	if hit_detection_area:
+		hit_detection_area.monitoring = false
+	
+	# Play destruction effects
+	animated_sprite.play("explode")  # Add this animation to your sprite
+	await animated_sprite.animation_finished
+	
+	queue_free()
