8acf96f85303201e435215a2717a4378af042f91
ahpham123 <ahpham123@csu.fullerton.edu>
2025-04-13 12:58:45 -0700

    finish global

--- globals.gd
+++ globals.gd
@@ -3,3 +3,12 @@ extends Node
 var CurryDebrisDestroyed: bool
 var BallDebrisDestroyed: bool
 var RocketDebrisDestroyed: bool
+var StudDebrisDestroyed: bool
+var SatelliteisDestroyed: bool
+var AppleisDestroyed: bool
+var BananaisDestroyed: bool
+var ShaqisDestroyed: bool
+var BottleisDestroyed: bool
+var TurtisDestroyed: bool
+var TrashisDestroyed: bool
+var BatteryisDestroyed: bool
